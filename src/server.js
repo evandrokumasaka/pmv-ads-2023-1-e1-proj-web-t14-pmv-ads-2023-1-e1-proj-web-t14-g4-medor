7e1c8a1cabd841d811856a078b172c9b7affa52d
evandrokumasaka <evandro.kumasaka@gmail.com>
2023-05-03 03:01:51 +0000

    Estrutura inicial

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// app.use(express.static('public'));
+app.use(express.static(path.join('./', 'public')));
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
