a30ba4e0638684269552c71f0f942b97778d0d1a
Easy Control <dev@easycontrolonline.com.br>
2023-06-11 17:35:16 -0300

    ES6

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 
 // app.use(express.static('public'));
-app.use(express.static(path.join('./', 'public')));
+app.use(express.static(path.join('./src', 'public')));
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
